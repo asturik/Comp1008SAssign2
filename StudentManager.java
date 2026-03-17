e08fb136de15894742276024a00ca514fef06bcb
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-03-17 15:03:25 -0400

    Created ArrayList to store student objects

--- StudentManager.java
+++ StudentManager.java
@@ -2,3 +2,6 @@ import java.util.ArrayList;
 
 // This class manages the list of students
 public class StudentManager {
+
+  // ArrayList to store all student objects
+  private ArrayList<Student> students = new ArrayList<>();
